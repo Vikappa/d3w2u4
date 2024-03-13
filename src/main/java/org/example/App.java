3e9b243d083ffdf36417f43d8b9dac4b593930e8
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-03-13 22:30:14 +0100

    Fine esercizi

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import org.example.entities.Customer;
+import org.example.entities.Order;
 import org.example.entities.Product;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 import java.util.stream.Stream;
 
@@ -11,22 +15,174 @@ public class App
 {
     public static void main( String[] args ) {
 
+///////////////////CREO UNO STREAM
+        System.out.println("--------------------------- STREAM DI PROVA ---------------------------");
+
         Stream<Product> randomProductsStream = Stream.generate(() -> {
-            Product newProd = Product.createRandomProduct();
+            Product newProd = Product.sortRandomProd();
             return newProd;
-        }).limit(10);
+        }).limit(3);
 
         randomProductsStream.forEach(System.out::println);
 
+        /////////ESERCIZIO 1
+        //////CREO UNO STREAM DI 10 PRODOTTI E LO FILTRO IN UN UNA LISTA
+        System.out.println("--------------------------- ESERCIZIO 1 ---------------------------");
+        System.out.println("--------------------------- BOOKS > 100 ---------------------------");
+
         List<Product> expensiveBooks = new ArrayList<>();
 
-        randomProductsStream.filter(p -> p.getPrice() > 1000).forEach(expensiveBooks::add);
+        Stream<Product> randomProductsStreamEs1 = Stream.generate(() -> {
+            Product es1 = Product.sortRandomProd();
+            return es1;
+        }).limit(15);
+
+        randomProductsStreamEs1.filter(p -> ((p.getPrice() > 100) && (Objects.equals(p.getCategory(), "Books")))).forEach(expensiveBooks::add);
 
         expensiveBooks.forEach(System.out::println);
 
+        //////////////////////////ESERCIZIO 2
+        /////////////////// CREO 3 ORDINI USANDO i metodi di classe per generare prodotti mockup
+        System.out.println("--------------------------- ESERCIZIO 2 ---------------------------");
+        System.out.println("----------------- ORDINI CON PRODOTTI PER BAMBINI -----------------");
+        Random random = new Random();
+        ArrayList<Product> carrello1 = new ArrayList<>();
+        carrello1.add(Product.sortRandomProd());
+        carrello1.add(Product.sortRandomProd());
+        carrello1.add(Product.sortRandomProd());
+        carrello1.add(Product.sortRandomProd());
+        carrello1.add(Product.sortRandomProd());
+        Order order1 = new Order(random.nextLong(), "in progress", LocalDate.now(), LocalDate.now().plusDays(10), carrello1, Customer.createRandomCustomer());
+
+        ArrayList<Product> carrello2 = new ArrayList<>();
+        carrello2.add(Product.sortRandomProd());
+        carrello2.add(Product.sortRandomProd());
+        carrello2.add(Product.sortRandomProd());
+        carrello2.add(Product.sortRandomProd());
+        carrello2.add(Product.sortRandomProd());
+        carrello2.add(Product.sortRandomProd());
+        carrello2.add(Product.sortRandomProd());
+        carrello2.add(Product.sortRandomProd());
+        carrello2.add(Product.sortRandomProd());
+        carrello2.add(Product.sortRandomProd());
+        Order order2 = new Order(random.nextLong(), "in progress", LocalDate.now(), LocalDate.now().plusDays(15), carrello2, Customer.createRandomCustomer());
+
+        ArrayList<Product> carrello3 = new ArrayList<>();
+        carrello3.add(Product.sortRandomProd());
+        carrello3.add(Product.sortRandomProd());
+        carrello3.add(Product.sortRandomProd());
+        carrello3.add(Product.sortRandomProd());
+        Order order3 = new Order(random.nextLong(), "Delivered", LocalDate.now(), LocalDate.now(), carrello3, Customer.createRandomCustomer());
+
+        /// TRASFORMO GLI ORDINI IN STREAM
+        Stream<Order> orderStream = Stream.of(order1, order2, order3);
+
+        ArrayList<Order> babyOrder = new ArrayList<>();
+
+        // ORDINI CON PRODOTTI APPARTENENTI ALLA CATEGORIA BABY
+        orderStream.forEach(order -> {
+            if(order.getProducts().stream().anyMatch(prod -> Objects.equals(prod.getCategory(), "Baby"))){
+                babyOrder.add(order);
+            }
+        });
+
+        babyOrder.forEach(System.out::println);
+
+        //////////////////////////ESERCIZIO 3
+        /////////////////// CREO 3 ORDINI USANDO i metodi di classe per generare prodotti mockup
+        System.out.println("--------------------------- ESERCIZIO 3 ---------------------------");
+        System.out.println("---------------- SCONTI SUI PRODOTTI CATEGORY BOYS ----------------");
+
+        //GENERO UNO STREAM DI 15 PRODOTTI
+        Stream<Product> randomProductsStreamEs3 = Stream.generate(() -> {
+            Product es1 = Product.sortRandomProd();
+            return es1;
+        }).limit(15);
+
+        //FILTRO I PRODOTTI E NE RIDUCO IL PREZZO
+
+        randomProductsStreamEs3.forEach(product -> {
+            if(Objects.equals(product.getCategory(), "Boys")){
+                System.out.println("Prodotto della categoria Boys trovato, prezzo ridotto da " + product.getPrice() + " a " + (product.getPrice()*0.9));
+                product.setPrice(product.getPrice()*0.9);
+            }
+        });
+
+        //////////////////////////ESERCIZIO 4
+        /////////////////// CREO UNO STREAM DI MOLTI CLIENTI PER AVERE MOLTI CASI TRA CUI FILTARE
+        System.out.println("--------------------------- ESERCIZIO 4 ---------------------------");
+        System.out.println("--- PRODOTTI DI ORDINI CON DATA TRA 01/02/21 e 01/04/22 E TIER2 ---");
+
+        ArrayList<Product> carrelloEse4N1 = new ArrayList<>();
+        carrelloEse4N1.add(Product.sortRandomProd());
+        carrelloEse4N1.add(Product.sortRandomProd());
+        carrelloEse4N1.add(Product.sortRandomProd());
+        carrelloEse4N1.add(Product.sortRandomProd());
+        carrelloEse4N1.add(Product.sortRandomProd());
+        carrelloEse4N1.add(Product.sortRandomProd());
+        carrelloEse4N1.add(Product.sortRandomProd());
+        Customer customr1Es4 = new Customer(random.nextLong(), "Vincenzo", 2);
+        Order orderEs4N1 = new Order(random.nextLong(), "in progress", LocalDate.of(2022, 2, 4), LocalDate.of(2021, 2, 10), carrelloEse4N1, customr1Es4);
+
+        ArrayList<Product> carrelloEse4N2 = new ArrayList<>();
+        carrelloEse4N2.add(Product.sortRandomProd());
+        carrelloEse4N2.add(Product.sortRandomProd());
+        carrelloEse4N2.add(Product.sortRandomProd());
+        carrelloEse4N2.add(Product.sortRandomProd());
+        carrelloEse4N2.add(Product.sortRandomProd());
+        carrelloEse4N2.add(Product.sortRandomProd());
+        carrelloEse4N2.add(Product.sortRandomProd());
+        Customer customr2Es4 = new Customer(random.nextLong(), "Alberto", 3);
+        Order orderEs4N2 = new Order(random.nextLong(), "in progress", LocalDate.of(2022, 2, 4), LocalDate.of(2021, 2, 10), carrelloEse4N1, customr2Es4);
+
+        ArrayList<Product> carrelloEse4N3 = new ArrayList<>();
+        carrelloEse4N3.add(Product.sortRandomProd());
+        carrelloEse4N3.add(Product.sortRandomProd());
+        carrelloEse4N3.add(Product.sortRandomProd());
+        carrelloEse4N3.add(Product.sortRandomProd());
+        carrelloEse4N3.add(Product.sortRandomProd());
+        carrelloEse4N3.add(Product.sortRandomProd());
+        carrelloEse4N3.add(Product.sortRandomProd());
+        Customer customr3Es4 = new Customer(random.nextLong(), "Vincenzo", 2);
+        Order orderEs4N3 = new Order(random.nextLong(), "in progress", LocalDate.of(2022, 2, 4), LocalDate.of(2021, 2, 10), carrelloEse4N1, customr3Es4);
+
+        ArrayList<Product> carrelloEse4N4 = new ArrayList<>();
+        carrelloEse4N4.add(Product.sortRandomProd());
+        carrelloEse4N4.add(Product.sortRandomProd());
+        carrelloEse4N4.add(Product.sortRandomProd());
+        carrelloEse4N4.add(Product.sortRandomProd());
+        carrelloEse4N4.add(Product.sortRandomProd());
+        carrelloEse4N4.add(Product.sortRandomProd());
+        carrelloEse4N4.add(Product.sortRandomProd());
+        Customer customr4Es4 = new Customer(random.nextLong(), "Vincenzo", 2);
+        Order orderEs4N4 = new Order(random.nextLong(), "in progress", LocalDate.of(2022, 2, 4), LocalDate.of(2021, 2, 10), carrelloEse4N1, customr4Es4);
+
+        ArrayList<Product> carrelloEse4N5 = new ArrayList<>();
+        carrelloEse4N5.add(Product.sortRandomProd());
+        carrelloEse4N5.add(Product.sortRandomProd());
+        carrelloEse4N5.add(Product.sortRandomProd());
+        carrelloEse4N5.add(Product.sortRandomProd());
+        carrelloEse4N5.add(Product.sortRandomProd());
+        carrelloEse4N5.add(Product.sortRandomProd());
+        carrelloEse4N5.add(Product.sortRandomProd());
+        Customer customr5Es4 = new Customer(random.nextLong(), "Vincenzo", 2);
+        Order orderEs4N5 = new Order(random.nextLong(), "in progress", LocalDate.of(2022, 2, 4), LocalDate.of(2021, 2, 10), carrelloEse4N1, customr5Es4);
+
+        ///GENERO LO STREAM A PARTIRE DAGLI ORDINI SCRITTI A MANO
+
+        Stream<Order> orderStreamEs4 = Stream.of(orderEs4N1, orderEs4N2, orderEs4N3, orderEs4N4, orderEs4N5);
 
 
+        // ORDINI CON DATA TRA 1/02/22 e 01/04/22 DI CLIENTI TIER2
+        ArrayList<Order> listaFinaleEs4 =new ArrayList<>();
 
+        orderStreamEs4.filter(order ->
+                !order.getOrderDate().isBefore(LocalDate.of(2022, 2, 1)) &&
+                        !order.getOrderDate().isAfter(LocalDate.of(2022, 4, 1)) &&
+                        order.getCustomer().getTier() == 2).forEach(listaFinaleEs4::add);
 
+        listaFinaleEs4.forEach(System.out::println);
     }
+
+
 }
--- src/main/java/org/example/entities/Product.java
+++ src/main/java/org/example/entities/Product.java
@@ -24,7 +24,7 @@ public class Product {
                     ", price=" + price +
                     '}';
         }
-    public static Product createRandomProduct() {
+    public static Product sortRandomProd() {
         long newProdId = new Random().nextLong();
         ProductInfo productInfo = ProductInfo.getRandomProductInfo();
         return new Product(newProdId, productInfo.name, productInfo.category, productInfo.price);
@@ -46,16 +46,88 @@ public class Product {
         return price;
     }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     public void setId(long id) {
         this.id = id;
     }
 
     public enum ProductInfo {
+        GARDENING_TOOLS_SET("Gardening Tools Set", "Gardening", 59.99),
+        GARDENING_GLOVES("Durable Gardening Gloves", "Gardening", 15.99),
+        PLANT_FERTILIZER("Organic Plant Fertilizer", "Gardening", 29.99),
+        KITCHEN_KNIFE_SET("Professional Kitchen Knife Set", "Kitchen", 99.99),
+        ESPRESSO_MACHINE("Espresso Coffee Machine", "Kitchen", 499.99),
+        BLENDER("High-Speed Blender", "Kitchen", 199.99),
+        SMART_FRIDGE("Smart Refrigerator", "Electronics", 2000.00),
+        VACUUM_ROBOT("Robotic Vacuum Cleaner", "Electronics", 299.99),
+        SMART_TV("Smart TV 55' 4K", "Electronics", 799.99),
+        WIRELESS_SPEAKER("Wireless Bluetooth Speaker", "Electronics", 149.99),
+        BOYS_ACTION_FIGURE("Superhero Action Figure", "Boys", 14.99),
+        BOYS_REMOTE_CONTROL_CAR("Remote Control Car", "Boys", 29.99),
+        BOYS_LEGO_SET("Space Explorer Lego Set", "Boys", 59.99),
+        BOYS_DIGITAL_WATCH("Digital Sports Watch", "Boys", 19.99),
+        BOYS_BASKETBALL("Junior Basketball", "Boys", 9.99),
+        BOYS_SKATEBOARD("Skateboard with Cool Design", "Boys", 39.99),
+        BOYS_VIDEO_GAME("Popular Video Game", "Boys", 59.99),
+        BOYS_GRAPHIC_TSHIRT("Graphic T-Shirt", "Boys", 12.99),
+        BOYS_BOOK_SERIES("Adventure Book Series", "Boys", 25.99),
+        BOYS_PUZZLE_GAME("3D Puzzle Game", "Boys", 15.99),
+        BOYS_SCIENCE_KIT("Young Scientist Kit", "Boys", 24.99),
+        BOYS_DRONE("Mini Drone", "Boys", 49.99),
+        BOYS_BIKE_HELMET("Cool Bike Helmet", "Boys", 29.99),
+        BOYS_SNEAKERS("High-Top Sneakers", "Boys", 34.99),
+        BOYS_SWIMMING_GOGGLES("Swimming Goggles", "Boys", 9.99),
+        BOYS_CAMPING_TENT("Backyard Camping Tent", "Boys", 49.99),
+        BOYS_TOOL_SET("Junior Tool Set", "Boys", 19.99),
+        BOYS_WALKIE_TALKIES("Walkie Talkies", "Boys", 20.99),
+        BOYS_MAGIC_TRICKS_SET("Magic Tricks Set", "Boys", 22.99),
+        BOYS_MODEL_AIRPLANE_KIT("Model Airplane Kit", "Boys", 18.99),
+        EBOOK_READER("E-Book Reader", "Electronics", 129.99),
+        FITNESS_TRACKER("Fitness Tracker Watch", "Sports", 59.99),
+        MOUNTAIN_BIKE("Mountain Bike", "Sports", 489.99),
+        YOGA_MAT("Eco-Friendly Yoga Mat", "Sports", 29.99),
+        RUNNING_SHOES("Running Shoes", "Sports", 99.99),
+        CAMPING_TENT("Four-Person Camping Tent", "Outdoor", 199.99),
+        SLEEPING_BAG("Warm Sleeping Bag", "Outdoor", 89.99),
+        PORTABLE_GRILL("Portable Grill", "Outdoor", 129.99),
+        ANTIQUE_CLOCK("Antique Wall Clock", "Home Decor", 325.99),
+        WALL_ART_PRINT("Abstract Wall Art Print", "Home Decor", 45.99),
+        DECORATIVE_VASE("Ceramic Decorative Vase", "Home Decor", 70.50),
+        LED_DESK_LAMP("LED Desk Lamp", "Office Supplies", 29.99),
+        ERGONOMIC_OFFICE_CHAIR("Ergonomic Office Chair", "Office Supplies", 249.99),
+        WIRELESS_CHARGING_PAD("Wireless Charging Pad", "Electronics", 60.00),
+        HANDHELD_STEAM_CLEANER("Handheld Steam Cleaner", "Home Appliances", 89.99),
+        SMART_HOME_HUB("Smart Home Hub", "Electronics", 99.99),
+        ELECTRIC_SCOOTER("Electric Scooter", "Transportation", 399.99),
+        SOLAR_POWERED_BACKPACK("Solar Powered Backpack", "Tech Gadgets", 120.00),
+        AUTOMATIC_PET_FEEDER("Automatic Pet Feeder", "Pet Supplies", 59.99),
+        WATERPROOF_ACTION_CAMERA("Waterproof Action Camera", "Electronics", 159.99),
+        DIGITAL_ART_TABLET("Digital Art Tablet", "Creative Tech", 199.99),
+        VR_HEADSET("VR Headset", "Gaming", 299.99),
+        BOARD_GAME_CLASSIC("Classic Board Game Collection", "Games", 49.99),
+        POCKET_PROJECTOR("Pocket Projector", "Electronics", 289.99),
+        NOISE_CANCELLING_HEADPHONES("Noise Cancelling Headphones", "Tech Gadgets", 349.99),
+        PORTABLE_ESPRESSO_MAKER("Portable Espresso Maker", "Outdoor", 64.99),
+        TRAVEL_GUIDE_BOOK("Ultimate World Travel Guide", "Books", 29.99),
+        SUSTAINABLE_WATER_BOTTLE("Sustainable Water Bottle", "Eco-Friendly", 24.99),
+        ORGANIC_SKIN_CARE_SET("Organic Skin Care Set", "Beauty", 59.99),
+        HANDCRAFTED_JEWELRY_BOX("Handcrafted Jewelry Box", "Crafts", 45.00),
+        DESIGNER_SUNGLASSES("Designer Sunglasses", "Fashion", 175.99),
+        LUXURY_WATCH("Luxury Watch", "Accessories", 450.00),
+        HIGH_PERFORMANCE_ROUTER("High Performance Router", "Tech Gadgets", 199.99),
         BOOK_HOBBIT("The Hobbit", "Books", 10.99),
         BOOK_HP("Harry Potter Series", "Books", 29.99),
         BOOK_1984("1984", "Books", 8.99),
         BOOK_GOT("Game of Thrones", "Books", 19.99),
         BOOK_SAPIENS("Sapiens", "Books", 14.99),
+        BOOK_LUXURY_EDITION("Encyclopedia Luxuria", "Books", 250.00),
+        BOOK_ART_HISTORY("The History of Art", "Books", 120.99),
+        BOOK_SCIENCE_COMPENDIUM("Compendium of Science", "Books", 200.00),
+        BOOK_SPACE_EXPLORATION("Space Exploration: A Visual History", "Books", 105.00),
+        BOOK_MEDICAL_TEXTBOOK("Advanced Medical Textbook", "Books", 180.99),
         BABY_CAR_SEAT("Convertible Car Seat", "Baby", 199.99),
         BABY_STROLLER("Lightweight Stroller", "Baby", 149.99),
         BABY_DIAPERS("Disposable Diapers", "Baby", 39.99),
@@ -71,6 +143,7 @@ public class Product {
         FOOD_TEABAGS("Teabags", "Food", 4.99),
         FOOD_OLIVE_OIL("Olive Oil", "Food", 10.99),
         FOOD_PASTA("Pasta", "Food", 1.99);
+
 
         final String name;
         final String category;
